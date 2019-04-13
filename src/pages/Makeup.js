683703ce88474a83524fba7907cdebdebd4e0d49
Julia Yang <jwy37@cornell.edu>
2019-04-13 01:09:53 -0400

    big react calendar for makeup

--- src/pages/Makeup.js
+++ src/pages/Makeup.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = BigCalendar.momentLocalizer(moment)
+
+const events = [
+  {
+    id: 0,
+    title: 'All Day Event very long title',
+    allDay: true,
+    start: new Date(2019, 4, 10),
+    end: new Date(2019, 4, 11),
+  }
+]
 
 const Makeup = () => (
-  <div>
+  <div class="container">
     Makeup
+    <div>
+      <BigCalendar
+        localizer={localizer}
+        events={events}
+        defaultDate={new Date(2019, 4, 1)}
+        startAccessor="startDate"
+        endAccessor="endDate"
+      />
+    </div>
   </div>
 )
 
