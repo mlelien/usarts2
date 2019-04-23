70587ccfab80258bd7ddba07104e4df6378ecf47
mlelien <emily.c.lien@gmail.com>
2019-04-22 23:58:45 -0400

    fixed styling

--- src/pages/Absence.js
+++ src/pages/Absence.js
@@ -15,7 +15,7 @@ class Absence extends Component {
       	<div className="container">
        		<div className="title">Mark an Absence</div>
         		<form>
-      			<div className='row'>
+      				<div className='row'>
 					<div className='row-item'>
 						<label className='input-group'>
 						<span>Absence Date</span>
@@ -45,67 +45,67 @@ class Absence extends Component {
 						</div>
 				  	</div>
           </div>
-					<div className='row'>
-						<div className='row-item'>
-						<label className='input-group'>
-							<span>Last Name</span>
-							<input
-								className='input'
-								type='text'
-							/>
-						</label>
-				  	</div>
-						<div className='row-item'>
-						<label className='input-group'>
-							<span>First Name</span>
-							<input
-								className='input'
-								type='text'
-							/>
-						</label>
-				  	</div>
-					  	<div className='row-item'>
-						<label className='input-group'>
-							<span>Student ID (optional)</span>
-							<input
-								className='input'
-								type='text'
-							/>
-						</label>
-				  	</div>
-					</div>
-					<div className='row'>
-						<div className='row-item'>
-							<label className='input-group'>
-								<span>Room #</span>
-								<select className='input'>
-									<option>112</option>
-									<option>113</option>
-								</select>
-							</label>
-						</div>
-						<div className='row-item'>
-							<label className='input-group'>
-								<span>Class Time</span>
-								<select className='input'>
-									<option>10:00 AM</option>
-									<option>11:00 AM</option>
-								</select>
-							</label>
-						</div>
-						<div className='row-item'>
-							<label className='input-group'>
-								<span>School pickup (optional)</span>
-								<span className='subtitle'>If your child usually gets picked up from school, please select the school here:</span>
-								<select className='input'>
-									<option>Rachel Carson</option>
-									<option>Holmes</option>
-								</select>
-							</label>
-						</div>
-					</div>
-					<button type='button'>+ add child</button>
-				</form>
+							<div className='row'>
+								<div className='row-item'>
+								<label className='input-group'>
+									<span>Last Name</span>
+									<input
+										className='input'
+										type='text'
+									/>
+								</label>
+								</div>
+								<div className='row-item'>
+								<label className='input-group'>
+									<span>First Name</span>
+									<input
+										className='input'
+										type='text'
+									/>
+								</label>
+								</div>
+									<div className='row-item'>
+								<label className='input-group'>
+									<span>Student ID (optional)</span>
+									<input
+										className='input'
+										type='text'
+									/>
+								</label>
+								</div>
+							</div>
+							<div className='row'>
+								<div className='row-item'>
+									<label className='input-group'>
+										<span>Room #</span>
+										<select className='input'>
+											<option>112</option>
+											<option>113</option>
+										</select>
+									</label>
+								</div>
+								<div className='row-item'>
+									<label className='input-group'>
+										<span>Class Time</span>
+										<select className='input'>
+											<option>10:00 AM</option>
+											<option>11:00 AM</option>
+										</select>
+									</label>
+								</div>
+								<div className='row-item'>
+									<label className='input-group'>
+										<span>School pickup (optional)</span>
+										<span className='subtitle'>If your child usually gets picked up from school, please select the school here:</span>
+										<select className='input'>
+											<option>Rachel Carson</option>
+											<option>Holmes</option>
+										</select>
+									</label>
+								</div>
+							</div>
+							<button type='submit'>+ add child</button>
+						</form>
         	<p><a href="/absence-pending">pending</a></p>
         	<p><a href='/absence-confirmation'>confirmation</a></p>
       </div>
