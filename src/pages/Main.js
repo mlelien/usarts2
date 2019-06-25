5488c6748bcefd8666b1298bbaf105a49cd8f017
mlelien <emily.c.lien@gmail.com>
2019-06-24 21:43:04 -0400

    debugging

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,19 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/jsx-closing-tag-location */
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Main = () => (
   <div className='container'>
     <h4 className='mb-3'>Welcome to the USArts Absence/Makeup scheduler!</h4>
     <p>Please submit an absence request first before scheduling a makeup.</p>
     <div className="d-flex justify-content-start">
-      <button type='button' className='mr-3' href='/absence'>Absence</button>
-      <button type='button' href='/makeup'>Makeup</button>
+      <Link to='/absence'>
+        <button type='button' className='mr-3'>Absence</button>
+      </Link>
+      <Link to='/makeup'>
+        <button type='button'>Makeup</button>
+      </Link>
     </div>
   </div>
 )
