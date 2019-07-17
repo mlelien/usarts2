602d40678b45edabdab2f14532385ae2e4862a71
mlelien <emily.c.lien@gmail.com>
2019-07-16 22:22:17 -0400

    button

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,20 +2,32 @@
 /* eslint-disable react/jsx-closing-tag-location */
 import React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
-const Main = () => (
-  <div className='container'>
-    <h4 className='mb-3'>Welcome to the USArts Absence/Makeup scheduler!</h4>
-    <p>Please submit an absence request first before scheduling a makeup.</p>
-    <div className="d-flex justify-content-start">
-      <Link to='/absence'>
-        <button type='button' className='mr-3'>Absence</button>
-      </Link>
-      <Link to='/makeup'>
-        <button type='button'>Makeup</button>
-      </Link>
+const Main = () => {
+  const sendEmail = () => {
+    axios.post('/api/sendConfirmation', {
+      parentEmail: 'emily.c.lien@gmail.com',
+      subject: 'Absence Confirmation',
+      text: 'Thanks for scheduling an absence!',
+    })
+  }
+
+  return (
+    <div className='container'>
+      <button className='btn btn-warning' onClick={sendEmail}>Send Email</button>
+      <h4 className='mb-3'>Welcome to the USArts Absence/Makeup scheduler!</h4>
+      <p>Please submit an absence request first before scheduling a makeup.</p>
+      <div className="d-flex justify-content-start">
+        <Link to='/absence'>
+          <button type='button' className='mr-3'>Absence</button>
+        </Link>
+        <Link to='/makeup'>
+          <button type='button'>Makeup</button>
+        </Link>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Main
